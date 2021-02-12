9afa11f0c40d5a1a16f536408310edcc1fe31f52
DiegoTCK <djegotck@ciencias.unam.mx>
2021-02-11 23:59:18 -0600

    Implementación del método palabraNarbol

--- Trie.hs
+++ Trie.hs
@@ -2,5 +2,17 @@ module Trie where
 
 -- Inicio del proyecto.
 
--- Definicion del arbol para el Trie
+{- Definicion del arbol para el Trie
+-- En si el arbol es la estructura que usare para la verificacion de las palabras. 
+-}
 data Arbol = Raiz [Arbol] | Flor Char [Arbol] | Hoja deriving(Eq, Show)
+
+
+
+{- Seccion de funciones auxiliares -}
+
+-- Funcion auxiliar palabraNarbol transforma una lista de Char a un Arbol
+palabraNarbol :: [Char] -> Arbol
+palabraNarbol [] = Hoja
+palabraNarbol (x:xs) = Flor x [palabraNarbol xs]
+
